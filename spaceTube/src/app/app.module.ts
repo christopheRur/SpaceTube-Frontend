fd7b7d844fbe9ddadfb6c6a9305f07aca76e0fca
Rurangwa <chrisruu@yahoo.com>
2023-11-16 23:07:28 +0800

    updates on ui

--- spaceTube/src/app/app.module.ts
+++ spaceTube/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SpaceTubeComponent } from './components/space-tube/space-tube.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card'
 @NgModule({
   declarations: [
     AppComponent,
     SpaceTubeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
